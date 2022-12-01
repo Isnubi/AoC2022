1933d4778f056e0ff1b791ffd155b8a3033ea93b
Louis GAMBART <louis.gambart77270@gmail.com>
2022-12-01 19:40:58 +0100

    flex on 3 lines

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,4 @@
 sum_list, current_sum, lines = [], 0, open('input').readlines()
-for line in lines:
-    if line.rstrip() == '':
-        sum_list.append(current_sum)
-        current_sum = 0
-    else: current_sum += int(line.rstrip())
+[exec("sum_list.append(current_sum)\ncurrent_sum = 0") if line.rstrip() == '' else exec("current_sum += int(line.rstrip())") for line in lines]
 print(f"Part one: {sorted(sum_list)[-1]}\nPart two: {sum(sorted(sum_list, reverse=True)[:3])}")
+
