392036df6b2e475ab60f4ec4a91f6194fee84ac4
Louis GAMBART <louis.gambart77270@gmail.com>
2022-12-03 17:07:56 +0100

    Day 3 - 12 to 8 lines

--- day3/main.py
+++ day3/main.py
@@ -1,12 +1,8 @@
 ord_list1,ord_list2,lines = [], [], open('input').readlines()
 for line in lines:
     for char in line[0:len(line)//2]:
-        if char in line[len(line)//2:]:
-            if 97 <= ord(char) <= 122: ord_list1.append(ord(char) - 96);break
-            elif 65 <= ord(char) <= 90: ord_list1.append(ord(char) - 38);break
+        if char in line[len(line)//2:]:ord_list1.append(ord(str.lower(char)) - ord("a") + 1 + (26 if str.isupper(char) else 0));break
 for i in range(0, len(lines), 3):
     for char in lines[i]:
-        if char in lines[i+1] and char in lines[i+2]:
-            if 97 <= ord(char) <= 122: ord_list2.append(ord(char) - 96);break
-            elif 65 <= ord(char) <= 90: ord_list2.append(ord(char) - 38);break
+        if char in lines[i+1] and char in lines[i+2]:ord_list2.append(ord(str.lower(char)) - ord("a") + 1 + (26 if str.isupper(char) else 0));break
 print(f"Part one: {sum(ord_list1)}\nPart two: {sum(ord_list2)}")
