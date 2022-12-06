2d5eeaa8da317a8939ab3461b4cb520114d10239
Louis GAMBART <louis.gambart77270@gmail.com>
2022-12-06 14:44:12 +0100

    Day 6 - 3 lines (ty chelinka)

--- day6/main.py
+++ day6/main.py
@@ -1,4 +1,3 @@
-for i in range(4, len(list(open('input').read().strip()))):
-    if len(list(open('input').read().strip())[i - 4:i]) == len(set(list(open('input').read().strip())[i - 4:i])): print(i);break
-for i in range(14, len(list(open('input').read().strip()))):
-    if len(list(open('input').read().strip())[i - 14:i]) == len(set(list(open('input').read().strip())[i - 14:i])): print(i);break
+for n in [4,14]:
+    for i in range(n, len(list(open('input').read().strip()))):
+        if len(list(open('input').read().strip())[i - n:i]) == len(set(list(open('input').read().strip())[i - n:i])): print(i);break
